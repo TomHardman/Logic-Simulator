b45f60ec103f6ef42e62401f6cb207a15d5cb9f2
Tom Hardman <th624@cam.ac.uk>
2023-05-16 10:43:19 +0100

    created new gui file

--- logsim/logsim.py
+++ logsim/logsim.py
@@ -22,7 +22,7 @@ from monitors import Monitors
 from scanner import Scanner
 from parse import Parser
 from userint import UserInterface
-from gui import Gui
+from gui_skeleton import Gui
 
 
 def main(arg_list):
