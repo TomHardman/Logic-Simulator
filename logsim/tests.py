6dae5c9c637dfb110b16db41951a9b54af64ea9c
Andrew Liang <al2008@cam.ac.uk>
2023-05-16 13:07:07 +0100

    final scanner tests

--- logsim/tests.py
+++ logsim/tests.py
@@ -12,24 +12,34 @@ def test_get_position(new_scanner):
     counter = 0
     while sym.type != new_scanner.EOF:
         sym = new_scanner.get_symbol()
-        print(sym.type)
-        print(new_scanner.current_character)
         counter+=1
-        print(counter)
         if sym.type == 2:
+            new_scanner.get_position(sym)
             arrows_list.append([sym.linenum, sym.linepos])
 
-        print(new_scanner.names.name_list)
-
+       
     print(counter)
     print(arrows_list)
 
 def new_scanner():
     names = Names()
-    newscanner = Scanner('logsim\scan_testinput.txt', names)
+    newscanner = Scanner('/Users/andrew/Documents/IIA Easter term projects/GF2 Software/Logic-Simulator/logsim/scan_testinput.txt', names)
     return newscanner
 
 new_scannertest = new_scanner()
 
 print(test_get_position(new_scannertest))
-print(new_scannertest.input_file.read())
+#print(new_scannertest.input_file.read())
+
+
+def get_position(self, symbol):
+    """Gets position of symbol which file object is currently pointing at"""
+    position = self.input_file.tell()
+    self.input_file.seek(0)
+
+    contents = self.input_file.read(position)
+    linenum = contents.count('\n') + 1
+    linepos = position - contents.rfind('\n') - 1
+    
+    symbol.linenum = linenum
+    symbol.linepos = linepos
